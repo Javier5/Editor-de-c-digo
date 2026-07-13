7769343dda4e322d83f7243e94a062788db93fd0
Javier5 <TU_CORREO_DE_GITHUB>
2026-07-13 18:58:29 -0500

    Core: Actualizada API de plugins via comando

--- src/core/language.rs
+++ src/core/language.rs
@@ -1,1 +1,14 @@
-pub trait EditorPlugin { fn name(&self) -> String; fn execute(&self, buffer: &mut String); } pub struct PluginManager { pub plugins: Vec<Box<dyn EditorPlugin>>, } impl PluginManager { pub fn new() -> Self { Self { plugins: Vec::new() } } }
+pub trait EditorPlugin {
+    fn name(&self) -> String;
+    fn execute(&self, buffer: &mut String);
+}
+
+pub struct PluginManager {
+    pub plugins: Vec<Box<dyn EditorPlugin>>,
+}
+
+impl PluginManager {
+    pub fn new() -> Self {
+        Self { plugins: Vec::new() }
+    }
+}
